19808b3c1da286f47ba2e0c9886ffd86430e206a
andrea <andrea.lacamera0@gmail.com>
2023-07-27 22:52:12 +0200

    Footer added

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Icon from "@iconify/svelte";
+
+
+</script>
+
+
+<footer class="bg-indigo-900 text-indigo-100 flex flex-col md:flex-row w-full">
+  <div class="w-full md:w-1/3 h-32">
+    LEFT AREA
+  </div>
+  <div class="w-full md:w-1/3 h-32">
+    CENTRAL AREA
+  </div>
+  <div class="w-full md:w-1/3 h-32">
+    RIGHT AREA
+  </div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import "../app.css"
   import Header from '$lib/components/Header.svelte'
+  import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <div class="max-w-[1280px] bg-indigo-100 m-auto min-h-screen p-4 md:p-8">
@@ -9,5 +10,5 @@
   <main>
     <slot />
   </main>
-  
+  <Footer />
 </div>
