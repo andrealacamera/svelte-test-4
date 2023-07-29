490263749cd17854b91325eb4d708d7cc0962c05
andrea <andrea.lacamera0@gmail.com>
2023-07-29 22:43:10 +0200

    Back-To-Top component

---
+++ src/lib/components/BackToTop.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import {fly} from 'svelte/transition';
+  import Icon from '@iconify/svelte';
+
+  let y: number;
+  $: visible = y > 50;
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+  }
+</script>
+
+<svelte:window bind:scrollY={y}></svelte:window>
+
+{#if visible}
+<div class="fixed bottom-2 right-2" transition:fly={{x:'100%'}}>
+  <button on:click={scrollToTop} class="p-4 rounded-xl bg-indigo-900 text-indigo-200 hover:text-indigo-50">
+    <Icon icon="bi:chevron-double-up" class="w-6 h-6" />
+  </button>
+</div>
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,14 @@
   import "../app.css"
   import Header from '$lib/components/Header.svelte'
   import Footer from "$lib/components/Footer.svelte";
+  import BackToTop from "$lib/components/BackToTop.svelte";
 </script>
 
-<div class="max-w-[1280px] bg-indigo-100 m-auto min-h-screen p-4 md:p-8">
-  
+<div class="max-w-[1280px] bg-indigo-100 m-auto min-h-screen p-4 md:p-8">  
   <Header />
   <main>
     <slot />
   </main>
   <Footer />
+  <BackToTop />
 </div>
