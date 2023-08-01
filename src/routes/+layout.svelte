9dac08cb804d6e6b2076c86abeabf06ecc79fb2f
andrea <andrea.lacamera0@gmail.com>
2023-08-01 22:07:57 +0200

    moved inline script from html to <svelte:head> element

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,38 @@
   import BackToTop from "$lib/components/BackToTop.svelte";
   import CookieBanner from "$lib/components/CookieBanner.svelte";
 </script>
+<svelte:head>
+  <script>
+    console.log("+LAYOUT.SVELTE <svelte:head> says hello!")
+  </script>
+  <script>
+    function changeTheme(e) {
+      console.log("THEME CHANGES...", e, !('theme' in localStorage));
+      window.location.reload();
+    }
+
+    localStorage.theme === 'dark' ||
+    (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+      ? document.documentElement.classList.add('dark')
+      : document.documentElement.classList.remove('dark');
+    
+    const darkModePreference = window.matchMedia("(prefers-color-scheme: dark)");
+    const lightModePreference= window.matchMedia("(prefers-color-scheme: light)");
+
+    darkModePreference.addEventListener("change", (e) => {
+      if (e.matches && !('theme' in localStorage)) {
+        changeTheme(1);
+      }
+    });
+    lightModePreference.addEventListener("change", (e) => {
+      if (e.matches && !('theme' in localStorage)) {
+        changeTheme(0);
+      }
+    }
+    );
+
+  </script>
+</svelte:head>
 <div class="max-w-[1280px] bg-indigo-100 text-indigo-900 dark:bg-indigo-900 dark:text-indigo-100 m-auto min-h-screen p-4 md:p-8">  
   <Header />
   <main>
